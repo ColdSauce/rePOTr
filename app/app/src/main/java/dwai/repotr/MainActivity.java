8998d6225d4691ea914995cf6ae1fd7a31b81395
Stefan <soccerfanatic1996@gmail.com>
2014-06-28 23:26:28 -0500

    Saving the app.

---
+++ app/app/src/main/java/dwai/repotr/MainActivity.java
@@ -0,0 +1,74 @@
+package dwai.repotr;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.*;
+
+import java.lang.reflect.Method;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+
+        GoogleMap map = ((MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+        map.getUiSettings().setScrollGesturesEnabled(false);
+
+        final Typeface mFont = Typeface.createFromAsset(getAssets(),
+                "fonts/proxima.ttf");
+        final ViewGroup mContainer = (ViewGroup) findViewById(
+                android.R.id.content).getRootView();
+        MainActivity.setAppFont(mContainer, mFont,true);
+    }
+
+
+    /**
+     * Recursively sets a {@link Typeface} to all
+     * {@link TextView}s in a {@link ViewGroup}.
+     */
+    public static final void setAppFont(ViewGroup mContainer, Typeface mFont, boolean reflect)
+    {
+        if (mContainer == null || mFont == null) return;
+
+        final int mCount = mContainer.getChildCount();
+
+        // Loop through all of the children.
+        for (int i = 0; i < mCount; ++i)
+        {
+            final View mChild = mContainer.getChildAt(i);
+            if (mChild instanceof TextView)
+            {
+                // Set the font if it is a TextView.
+                ((TextView) mChild).setTypeface(mFont);
+            }
+            else if (mChild instanceof ViewGroup)
+            {
+                // Recursively attempt another ViewGroup.
+                setAppFont((ViewGroup) mChild, mFont,true);
+            }
+            else if (reflect)
+            {
+                try {
+                    Method mSetTypeface = mChild.getClass().getMethod("setTypeface", Typeface.class);
+                    mSetTypeface.invoke(mChild, mFont);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+}
